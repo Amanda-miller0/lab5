08d31e0d101f2e72c69bed5ae3ed20484c3ebee1
Amanda <contactamanda.miller@gmail.com>
2026-04-13 21:53:03 -0500

    Add MovieForm component and AddMovieFormView page

---
+++ src/components/MovieForm.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <!-- Success message -->
+    <div v-if="successMessage" class="alert alert-success">
+      {{ successMessage }}
+    </div>
+
+    <!-- Error messages -->
+    <div v-if="errors.length > 0" class="alert alert-danger">
+      <ul class="mb-0">
+        <li v-for="(error, index) in errors" :key="index">{{ error }}</li>
+      </ul>
+    </div>
+
+    <form id="movieForm" @submit.prevent="saveMovie">
+      <div class="form-group mb-3">
+        <label for="title" class="form-label">Movie Title</label>
+        <input type="text" name="title" id="title" class="form-control" />
+      </div>
+
+      <div class="form-group mb-3">
+        <label for="description" class="form-label">Description</label>
+        <textarea name="description" id="description" class="form-control" rows="4"></textarea>
+      </div>
+
+      <div class="form-group mb-3">
